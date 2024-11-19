466f2e1a2da660af1f90a198c4ee82e1ea37ca72
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-20 01:21:52 +0900

    Remove redundant is_string check and cast view to string
    This commit removes the unnecessary check for is_string before processing the view. Instead, the view is directly cast to a string, ensuring type safety and simplifying the code. This change is also annotated to ignore specific PHPStan warnings.

--- tests/IntegrateTest.php
+++ tests/IntegrateTest.php
@@ -14,7 +14,6 @@ use PHPUnit\Framework\TestCase;
 use Ray\Di\Injector;
 
 use function assert;
-use function is_string;
 use function method_exists;
 use function ob_get_clean;
 use function ob_start;
@@ -79,8 +78,7 @@ class IntegrateTest extends TestCase
         $ro->setRenderer($this->renderer);
         assert(method_exists($ro, 'onGet'));
         $view = $ro->onGet();
-        assert(is_string($view));
-        $stream = $this->streamer->getStream($view);
+        $stream = $this->streamer->getStream((string) $view); // @phpstan-ignore-line
         rewind($stream);
         $view = stream_get_contents($stream);
         $this->assertSame($expected, $view);
